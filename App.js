9bbf5aa4bf76f4020bb753d870eebaa638513513
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-05-08 21:46:32 -0500

    [feature] - sign up styles & components & logic ui

--- App.js
+++ App.js
@@ -16,7 +16,11 @@ import {
   TextInput,
   TouchableOpacity,
   Animated,
-  Dimensions
+  Dimensions,
+  ScrollView,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Platform
 } from 'react-native';
 import { TypingAnimation } from 'react-native-typing-animation'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -32,11 +36,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   header: {
-    flex:1
+    flex: 1
   },
   footer: {
-    flex: 2,
-    padding: 20
+    marginHorizontal: 26,
+    marginBottom: 10
   },
   imageBackground: {
     flex: 1,
@@ -90,8 +94,13 @@ export default class App extends Component {
     this.state = {
       typingEmail: false,
       typingPassword:false,
-      animationLogin : new Animated.Value(width-40),
-      enable: true
+      typingPhoneNumber: false,
+      animationSignIn : new Animated.Value(width-40),
+      animationSignUp: new Animated.Value(width-50),
+      enable: true,
+      signUp: false,
+      signIn: true,
+      login: false
     }
   }
 
@@ -99,18 +108,27 @@ export default class App extends Component {
     if(value === 'email' ) {
       this.setState({
         typingEmail: true,
-        typingPassword: false
+        typingPassword: false,
+        typingPhoneNumber: false
+      })
+    } else if (value === 'password') {
+      this.setState({
+        typingEmail: false,
+        typingPassword: true,
+        typingPhoneNumber: false
       })
     } else {
       this.setState({
         typingEmail: false,
-        typingPassword: true
+        typingPassword: false,
+        typingPhoneNumber: true
       })
     }
   } 
   _animation = () => {
+    const { signIn, signUp } = this.state;
     Animated.timing(
-      this.state.animationLogin, {
+     this.state.animationSignIn, {
         toValue: 40,
         duration: 250
       }
@@ -119,7 +137,8 @@ export default class App extends Component {
       this.setState({
         enable: false,
         typingEmail: false,
-        typingPassword: false
+        typingPassword: false,
+        login: true
       })
     }), 150
   }
@@ -130,12 +149,24 @@ export default class App extends Component {
         style={{ marginRight: 25}}
       />
     )
-  } 
+  }
+  changeLoginView = () => {
+    const { signIn, signUp } = this.state;
+    this.setState({ 
+      signIn: !signIn,
+      signUp: !signUp,
+      enable: true
+    })
+  }
   render(){
-    const { animationLogin, typingEmail, typingPassword, enable } = this.state;
-    const width = animationLogin;
+    const { animationSignIn, animationSignUp, typingEmail, typingPassword, typingPhoneNumber,enable, signIn, signUp, login } = this.state;
+    const width = login && enable ? animationSignUp : signIn ? animationSignIn : animationSignUp ;
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView 
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+      > 
+      <View style={styles.container}>
       <StatusBar barStyle='light-content' />
       <View style={styles.header}>
         <ImageBackground
@@ -147,57 +178,124 @@ export default class App extends Component {
           <Text style={{color: '#212121'}}>Sign in to continue</Text>
         </ImageBackground>
       </View>
-      <View style={styles.footer}>
-        <Text style={[styles.title, { marginTop: 50 }]}>E-mail</Text>
-        <View style={styles.action}>
-          <TextInput
-            placeholder='your email...'
-            style={styles.textInput}
-            onFocus={() => this.focus('email')}
-            />
-            { typingEmail ? this.typing() : null}
-        </View>
-        <Text style={[styles.title, { marginTop: 20}]}>
-          Password
-        </Text>
-        <View style={styles.action}>
-          <TextInput 
-            secureTextEntry
-            placeholder='your password...'
-            style={styles.textInput}
-            onFocus={() => this.focus('password')}
-          />
-          { typingPassword ? this.typing() : null}
-          
-        </View>
-        <TouchableOpacity
-        onPress={() => this._animation()}>
-          <View style={styles.buttonContainer}>
-          <Animated.View style={[styles.animation, { width }]}>
-            { enable ? (
-              <Text style={styles.textLogin}>
-                Login
-              </Text>
-            ) : (
-              <Animatable.View
-                animation='bounceIn'
-                delay={50}
-              >
-                <FontAwesomeIcon 
-                  icon={ faCheck }
-                  style={{ color: '#ffffff' }}
-                />
-              </Animatable.View>
-            ) }
-          </Animated.View>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.footer}>
+        {signIn ? (
+          <>
+          <Text style={[styles.title, { marginTop: 50 }]}>E-mail</Text>
+          <View style={styles.action}>
+            <TextInput
+              placeholder='your email...'
+              placeholderTextColor='#cccccc'
+              keyboardType='email-address'
+              style={styles.textInput}
+              onFocus={() => this.focus('email')}
+              />
+              { typingEmail ? this.typing() : null}
           </View>
-        </TouchableOpacity>
+          <Text style={[styles.title, { marginTop: 20}]}>
+            Password
+          </Text>
+          <View style={styles.action}>
+            <TextInput 
+              secureTextEntry
+              placeholder='your password...'
+              placeholderTextColor='#cccccc'
+              style={styles.textInput}
+              onFocus={() => this.focus('password')}
+            />
+            { typingPassword ? this.typing() : null}
+            
+          </View>
+          <TouchableOpacity
+          onPress={() => {
+            this._animation()
+            }}>
+            <View style={styles.buttonContainer}>
+            <Animated.View style={[styles.animation, { width }]}>
+              { enable ? (
+                <Text style={styles.textLogin}>
+                  Login
+                </Text>
+              ) : (
+                <Animatable.View
+                  animation='bounceIn'
+                  delay={50}
+                >
+                  <FontAwesomeIcon 
+                    icon={ faCheck }
+                    style={{ color: '#ffffff' }}
+                  />
+                </Animatable.View>
+              ) }
+            </Animated.View>
+            </View>
+          </TouchableOpacity>
+          </>
+          ): (
+            <>
+          <Text style={[styles.title, { marginTop: 50 }]}>E-mail</Text>
+          <View style={styles.action}>
+            <TextInput
+              placeholder='your email...'
+              placeholderTextColor='#cccccc'
+              keyboardType='email-address'
+              style={styles.textInput}
+              onFocus={() => this.focus('email')}
+              />
+              { typingEmail ? this.typing() : null}
+          </View>
+          <Text style={[styles.title, { marginTop: 20}]}>
+            Password
+          </Text>
+          <View style={styles.action}>
+            <TextInput 
+              secureTextEntry
+              placeholder='your password...'
+              placeholderTextColor='#cccccc'
+              style={styles.textInput}
+              onFocus={() => this.focus('password')}
+            />
+            { typingPassword ? this.typing() : null}
+          </View>
+          <Text style={[styles.title, { marginTop: 20}]}>
+            Phone number
+          </Text>
+          <View style={styles.action}>
+            <TextInput 
+              keyboardType='phone-pad'
+              placeholder='your phone number...'
+              placeholderTextColor='#cccccc'
+              style={styles.textInput}
+              onFocus={() => this.focus('phoneNumber')}
+            />
+            { typingPhoneNumber ? this.typing() : null}
+          </View>
+          <TouchableOpacity
+          onPress={() => {
+            this.setState({
+              signUp: false,
+              signIn: true
+            })
+            }}>
+            <View style={[styles.buttonContainer, styles.animation]}>
+                <Text style={styles.textLogin}>
+                  Sign up
+                </Text>
+            </View>
+          </TouchableOpacity>
+          </>
+          )}
         <View style={styles.signUp}>
           <Text style={{ color: '#212121'}}>New User?</Text>
-          <Text style={{ color: '#4285F4' }}> Sign up</Text>
+          <TouchableOpacity onPress={() => this.changeLoginView()}>
+            <Text style={{ color: '#4285F4' }}>{signUp ? 'Login' : 'Sign up'}</Text>
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+      </ScrollView>
+      </View>
+    </KeyboardAvoidingView>
   );
   }
 };
