60b7b35117a27146dc7ea31663243b3cc911b76b
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-05-15 19:51:42 -0500

    [feature] - integrate unsplash

--- src/Scenes/Home.js
+++ src/Scenes/Home.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
 import { View, Text, Image, ScrollView, ImageBackground, StatusBar, StyleSheet, FlatList, SafeAreaView } from 'react-native'
+import firebase from '@react-native-firebase/app'
 import remoteConfig from '@react-native-firebase/remote-config';
 import { Post } from '../Components/Post'
+// ES Modules syntax
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({
+  accessKey: "OXxouhWwb4J81e_NVoaS5uqX-NTihadxGWFy8dAOMxA",
+  // Optionally you can also configure a custom header to be sent with every request
+  // Optionally if using a node-fetch polyfill or a version of fetch which supports the timeout option, you can configure the request timeout for all requests
+  timeout: 500 // values set in ms
+});
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -33,16 +43,28 @@ export default class Home extends Component {
     }
     async componentDidMount(){
         console.warn('this.state.data', this.state.data)
-        this.getPost();
-        const parameters = remoteConfig().getAll();
-        const data = {}
-        Object.keys(parameters).forEach((key) => {
-            data[key] = parameters[key].value
+        // this.getPost();
+        
+        // const parameters = remoteConfig().getAll();
+        // console.warn('parameters', parameters)
+        // const data = {}
+        // Object.entries(parameters).forEach(([key, parameter]) => {
+        //     data[key] = parameters[key].value
+        // });
+        // this.setState({
+        //     data: data
+        // })
+        // console.warn('data', data)
+        unsplash.photos.getRandomPhoto({ count: "10" })
+        .then((res)=> {
+            console.warn('res', res)
+            return res.json();
         })
-        this.setState({
-            data: data
+        .then(json => {
+            console.warn('json', json)
         })
-        console.warn('data', data)
+        .catch((error)=> console.warn('error', error))
+        
     }
     getPost = () => {
         const { postList } = this.state;
