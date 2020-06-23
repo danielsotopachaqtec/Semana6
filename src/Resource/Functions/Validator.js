1aab8c3416f5c082d6415350623deba09d62c09b
Daniel Alberto Soto Guillen <daniel.alberto.soto@LIM-WS1814.local>
2020-06-22 21:02:38 -0500

    [feature] - implement form payment methods && validator

--- src/Resource/Functions/Validator.js
+++ src/Resource/Functions/Validator.js
@@ -1,6 +1,10 @@
 const formatEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const formatPhone = /^\d{9}$/;
 const maxCharacterPhone = 9;
+const minCharacterCard = 15;
+const expireDate = 5;
+const numbers = /^\d+$/;
+const name = /^[a-zA-Z ]*$/;
 const formatPassword = /^(?=.*?[0-9])(?=.*?[A-Z])(?=.*?[#?!@$%^&*\-_]).{8,}$/;
 
 const Validator = (type, value) => {
@@ -10,7 +14,7 @@ const Validator = (type, value) => {
                 return { error: ''};
             }
             return {
-                error: 'Ingresa un formato valido. ej: usuario@dominio.com.',
+                error: 'Ingresa un formato válido. ej: usuario@dominio.com.',
                 value
             }
         case 'password': 
@@ -18,7 +22,7 @@ const Validator = (type, value) => {
                 return { error: ''};
             }
             return {
-                error: 'Minimo 8 caracteres, debe incluir como minimo caracteres especiales, numeros y una letra en mayuscula.',
+                error: 'Mínimo 8 caracteres, debe incluir como minimo caracteres especiales, numeros y una letra en mayuscula.',
                 value
             }
         case 'phoneNumber':
@@ -26,7 +30,39 @@ const Validator = (type, value) => {
                 return { error: '' }
             }
             return {
-                error: 'Ingresa un numero telefonico valido.',
+                error: 'Ingresa un número teléfonico valido.',
+                value
+            }
+        case 'cardNumber':
+            if(value.length >= minCharacterCard  ) {
+                return { error: ''}
+            }
+            return {
+                error: 'Ingresa un número de tarjeta válida',
+                value
+            }
+        case 'name':
+            if(name.test(value)) {
+                return { error: '' }
+            }
+            return {
+                error: 'por favor, ingrese solo letras',
+                value
+            }
+        case 'expireDate':
+            if(value.length === expireDate) {
+                return { error: '' }
+            }
+            return {
+                error: 'formato incorrecto',
+                value
+            }
+        case 'number':
+            if(numbers.test(value)) {
+                return { error: '' }
+            }
+            return {
+                error: 'formato incorrecto',
                 value
             }
         default:
--- src/Scenes/Checkout.js
+++ src/Scenes/Checkout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, FlatList, Dimensions, SafeAreaView, ScrollView, Platform } from 'react-native'
 import MenuFooter from '../Components/Menu/MenuFooter'
 import Button from '../Components/Forms/Button'
+import Input from '../Components/Forms/Input'
 import { CardPayment } from '../Components//ShoppingCart/CardPayment'
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
@@ -30,16 +31,19 @@ const styles = StyleSheet.create({
         color: '#cccccc',
         marginTop: 5
     },
-    title: {
-        fontSize: 20,
+    title: { 
         color: '#212121',
-        marginTop: 5,
-    },
+        fontWeight: 'bold'
+      },
     subtitle: {
         fontSize: 14,
         color: '#212121',
         marginTop: 5
     },
+    action: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#f2f2f2',
+      },
     colorTitle: {
         fontSize: 18,
         color: '#212121',
@@ -51,6 +55,11 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS == 'ios' ? 0 : 10,
         marginBottom: 10
     },
+    textInput: {
+        marginTop: 5,
+        paddingBottom: 5,
+        color: '#212121',
+      },
     productDetailsContainer: {
         flex: 1,
         flexDirection: 'row'
@@ -58,15 +67,21 @@ const styles = StyleSheet.create({
     productDetails: {
         flex: 0.5
     },
+    detailContainer: {
+        position: 'relative',
+        top: height * 0.30
+    },
     buyButtonContainer: {
-        flex: 0.5,
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        marginTop: 20
     },
     buyButton: {
+        width: '100%',
         backgroundColor: '#93278f',
-        height: width * 0.15,
-        paddingHorizontal: width * 0.10,
+        height: height * 0.05,
+        marginHorizontal: 30,
         borderRadius: 10,
         justifyContent: 'center',
         alignItems: 'center',
@@ -77,9 +92,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 15
     },
-    detailContainer: {
-        position: 'relative',
-        top: height * 0.30
+    secureCardData: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    expiresDate: {
+        flex: 0.6
+    },
+    dataCvv: {
+        marginLeft: 25,
+        flex: 0.3,
+    },
+    paymentContainer: {
+        marginHorizontal: 20
     }
 })
 
@@ -89,7 +114,11 @@ export default class ShoppingCart extends Component {
         this.state = {
             disabledButton: true,
             product: '',
-            selectedProduct: ''
+            selectedProduct: '',
+            cardNumber: '',
+            cardHolder: '',
+            expireDate: '',
+            cvv: ''
         }
     }
     componentDidMount(){
@@ -100,6 +129,51 @@ export default class ShoppingCart extends Component {
             selectedProduct: selectedProduct
         })
     }
+    sendPayment = () => {
+        console.warn('sendPayment')
+    }
+    onChangeText = (value, type) => {
+        if(type === 'cardNumber'){
+            console.warn('value.length onChange', value.length)
+            // if(value.length === 0) {
+            //     this.inputCardNumber.setState({
+            //         error: 'datos incorrectos.',
+            //         value
+            //     })
+            // }
+          this.setState({ 
+            cardNumber: this.inputCardNumber.state.value
+          })
+        }
+        if(type === 'cardHolder') {
+          this.setState({
+            cardHolder: this.inputCardHolder.state.value
+          })
+        }
+        if(type === 'expireDate') {
+          this.setState({
+            expireDate: this.inputExpireDate.state.value
+          })
+        }
+        if(type === 'cvv') {
+            this.setState({
+                cvv: this.inputCvv.state.value
+            })
+          }
+        if(value.length > 0 && (this.inputCardNumber.state.validate &&
+            this.inputCardHolder.state.validate &&
+            this.inputExpireDate.state.validate &&
+            this.inputCvv.state.validate)
+            ){
+          this.setState({
+            disabledButton: false
+          })
+        } else {
+            this.setState({
+                disabledButton: true
+              })
+        }
+      }
     removeProduct = () => {
         this.setState({
             product: '',
@@ -114,6 +188,7 @@ export default class ShoppingCart extends Component {
         })
     }
     render(){
+        const { cardNumber, cardHolder, expireDate, cvv, disabledButton } = this.state
         const { product, selectedProduct, paymentMethods } = this.props.route.params
         console.warn(product, selectedProduct, paymentMethods)
         return(
@@ -124,7 +199,78 @@ export default class ShoppingCart extends Component {
                                 item={paymentMethods}
                             />
                             <View style={styles.paymentContainer}>
-                                <Text>{'Form'}</Text>
+                            <>
+                                <View style={styles.action}>
+                                    <Input
+                                        label='Card number'
+                                        labelStyle={styles.title}
+                                        value={cardNumber}
+                                        ref={(ref) => this.inputCardNumber = ref}
+                                        type='cardNumber'
+                                        placeholder=''
+                                        placeholderTextColor='#cccccc'
+                                        TextInputStyle={styles.textInput}
+                                        keyboardType='numeric'
+                                        maxLength={16}
+                                        onChange={(value) => this.onChangeText(value, 'cardNumber')}
+                                    />
+                                </View>
+                                <View style={styles.action}>
+                                <Input 
+                                    label='Card holder'
+                                    labelStyle={styles.title}
+                                    value={cardHolder}
+                                    ref={(ref) => this.inputCardHolder = ref}
+                                    type='name'
+                                    placeholder=''
+                                    placeholderTextColor='#cccccc'
+                                    TextInputStyle={styles.textInput}
+                                    maxLength={60}
+                                    onChange={(value) => this.onChangeText(value, 'cardHolder')}
+                                    />
+                                </View>
+                                <View style={styles.secureCardData}>
+                                    <View style={[styles.expiresDate, styles.action]}>
+                                        <Input
+                                        label='Expire Date'
+                                        labelStyle={styles.title}
+                                        value={expireDate}
+                                        ref={(ref) => this.inputExpireDate = ref}
+                                        type='expireDate'
+                                        placeholder='XX/XX'
+                                        placeholderTextColor='#cccccc'
+                                        keyboardType='numeric'
+                                        onChange={(value) => this.onChangeText(value, 'expireDate')}
+                                        TextInputStyle={styles.textInput}
+                                        maxLength={5}
+                                        />
+                                    </View>
+                                    <View style={[styles.dataCvv, styles.action]}>
+                                        <Input
+                                        label='CVV'
+                                        labelStyle={styles.title}
+                                        value={cvv}
+                                        ref={(ref) => this.inputCvv = ref}
+                                        type='number'
+                                        placeholder='XXX'
+                                        placeholderTextColor='#cccccc'
+                                        keyboardType='numeric'
+                                        onChange={(value) => this.onChangeText(value, 'cvv')}
+                                        TextInputStyle={styles.textInput}
+                                        maxLength={3}
+                                        />
+                                    </View>
+                                </View>
+                                <View style={styles.buyButtonContainer}>
+                                    <Button 
+                                        onPressButton={this.sendPayment}
+                                        styleButton={styles.buyButton}
+                                        styleText={styles.textBuyButton}
+                                        title={'Realizar pago'}
+                                        disabled={disabledButton}
+                                        />
+                                </View>
+                                </>
                             </View>
                         </View>
                 </ScrollView>
